f3586d087756cf92e253f2880bdc99af21835e7b
everm1nd <everm1nd.mail@gmail.com>
2016-09-01 21:46:35 +0200

    Initial Commit

---
+++ timelapseTests/timelapseTests.swift
@@ -0,0 +1,36 @@
+//
+//  timelapseTests.swift
+//  timelapseTests
+//
+//  Created by Nikita Simakov on 01.09.16.
+//  Copyright © 2016 Nikita Simakov. All rights reserved.
+//
+
+import XCTest
+@testable import timelapse
+
+class timelapseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
