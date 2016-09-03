45699a67922b433a6d39f30d6f9798a5e19ca47b
everm1nd <everm1nd.mail@gmail.com>
2016-09-03 19:08:29 +0200

    added slideshow with start/stop function

--- timelapse/ViewController.swift
+++ timelapse/ViewController.swift
@@ -7,19 +7,84 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    var images: PHFetchResult!
+    var imageManager: PHImageManager!
+    var imageIndex = 0;
+    
+    var timer: NSTimer!
+    
+    var cachedImages = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        images = PHAsset.fetchAssetsWithMediaType(.Image, options: nil)
+        imageManager = PHImageManager()
+        warmupCache()
+        updateImageView() // update it once, becasue timer first fires after N miliseconds
+        startTimer()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func warmupCache() -> Void {
+        let frame = photoImageView.frame.size
+        let sizeFactor = CGFloat(2) // without this photos looks blurred on retina
+        let imageSize = CGSize(width: frame.width * sizeFactor,
+            height: frame.height * sizeFactor)
+        
+        images.enumerateObjectsUsingBlock { (object: AnyObject, index: Int, stop) -> Void in
+            let asset = object as! PHAsset
+            
+            let options = PHImageRequestOptions()
+            options.synchronous = true
+            options.deliveryMode = .HighQualityFormat
+            options.resizeMode = .Exact
+            
+            self.imageManager.requestImageForAsset(asset,
+                targetSize: imageSize,
+                contentMode: .AspectFill,
+                options: options,
+                resultHandler: {
+                    image, info in
+                    debugPrint(image)
+                    self.cachedImages.append((image as UIImage?)!)
+            })
+       }
+    }
+    
+    func updateImageView() -> Void {
+        print("\(imageIndex + 1)/\(images.count)")
+        let image = cachedImages[imageIndex]
+        self.photoImageView.image = image
+        imageIndex = nextImageIndex()
+    }
+    
+    func nextImageIndex() -> Int {
+        return ( imageIndex + 1 ) % ( images.count )
+    }
+    
+    @IBAction func imageTap(sender: UITapGestureRecognizer) {
+        if let timer = self.timer {
+            timer.invalidate()
+            self.timer = nil
+        } else {
+            startTimer()
+        }
+    }
+    
+    func startTimer() {
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "updateImageView", userInfo: nil, repeats: true)
+    }
 
 }
 
